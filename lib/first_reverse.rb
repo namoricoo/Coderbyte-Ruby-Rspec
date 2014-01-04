b9397c81aaa70a15d89fd62b522a3434de3503c9
Nate <namoricoo@hotmail.com>
2014-01-03 17:32:15 -0800

    Pass test for first_reverse test

--- lib/first_reverse.rb
+++ lib/first_reverse.rb
@@ -1,7 +1,10 @@
-# Using the Ruby language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
-def first_reverse(str)
+# Class definition
+class Reverse
+  def first_reverse(str)
 
-  # code goes here
-  return str          
+    # code goes here
+    return str          
+  end
 end
+
 
--- spec/first_reverse_spec.rb
+++ spec/first_reverse_spec.rb
@@ -1,7 +1,7 @@
 # # Using the Ruby language, have the function FirstReverse(str) take the str 
 # parameter being passed and return the string in reversed order.
 require 'spec_helper'
-require '../lib/first_reverse'
+require_relative '../lib/first_reverse.rb'
 
 describe Reverse do
 
