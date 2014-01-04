8d4c8427bfca857268de73552361184d8bb7bce2
Nate <namoricoo@hotmail.com>
2014-01-03 17:20:17 -0800

    initial commit first_reverse_spec class declaration

---
+++ lib/first_reverse.rb
@@ -0,0 +1,7 @@
+# Using the Ruby language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+def first_reverse(str)
+
+  # code goes here
+  return str          
+end
+
---
+++ spec/first_reverse_spec.rb
@@ -0,0 +1,9 @@
+# # Using the Ruby language, have the function FirstReverse(str) take the str 
+# parameter being passed and return the string in reversed order.
+require 'spec_helper'
+require '../lib/first_reverse'
+
+describe Reverse do
+
+end
+
