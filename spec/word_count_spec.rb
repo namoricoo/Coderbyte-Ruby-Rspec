27f6752068381fbb9223cd7cf0790456561c304d
Nate <namoricoo@hotmail.com>
2014-01-05 12:37:18 -0800

    word count spec

---
+++ spec/word_count_spec.rb
@@ -0,0 +1,14 @@
+# Using the Ruby language, have the function WordCount(str)
+# take the str string parameter being passed and return the
+# number of words the string contains 
+# (ie. "Never eat shredded wheat" would return 4).
+# Words will be separated by single spaces
+require 'spec_helper'
+require_relative '../lib/word_count.rb'
+describe 'word_count' do
+  it "total words in string Input = 'Hello World' Output = 2" do
+    input = 'Hello World'
+    output = 2
+    word_count(input).should == output
+  end
+end
