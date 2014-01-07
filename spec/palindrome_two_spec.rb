e17b638e0ce52d9ea188391ceb076650e9303828
Nate <namoricoo@hotmail.com>
2014-01-06 22:42:26 -0800

    spec for palindrome_two

---
+++ spec/palindrome_two_spec.rb
@@ -0,0 +1,17 @@
+# Using the Ruby language, have the function PalindromeTwo(str)
+# take the str parameter being passed and return the string true
+# if the parameter is a palindrome, (the string is the same
+# forward as it is backward) otherwise return the string false.
+# The parameter entered may have punctuation and symbols but
+# they should not affect whether the string is in fact a
+# palindrome. For example: "Anne, I vote more cars race
+# Rome-to-Vienna" should return true
+require 'spec_helper'
+require_relative '../lib/palindrome_two.rb'
+describe 'palindrome_two' do
+  it " Input = 'Noel - sees Leon' Output = 'true'" do
+    input = 'Noel - sees Leon'
+    output = true
+    palindrome_two(input).should == output
+  end
+end
