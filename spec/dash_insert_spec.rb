ee6bef5c739657fe1e80824d1b60652a4bc3c8df
Nate <namoricoo@hotmail.com>
2014-01-07 20:02:59 -0800

    add dash_inseert_spec

---
+++ spec/dash_insert_spec.rb
@@ -0,0 +1,13 @@
+# Using the Ruby language, have the function DashInsert(num) insert
+# dashes ('-') between each two odd numbers in num. For example: if
+# num is 454793 the output should be 4547-9-3. Don't count zero
+# as an odd number.
+ require 'spec_helper'
+ require_relative '../lib/dash_insert.rb'
+ describe 'dash_insert' do
+   it "insert dash after odd number Input = 99946Output = '9-9-946'" do
+     input = 99946
+     output = '9-9-946'
+     dash_insert(input).should == output
+   end
+ end 
