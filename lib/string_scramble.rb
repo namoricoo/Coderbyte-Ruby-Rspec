aad95a5b990f4fba0a5e7b2e35283772a650f041
Nate <namoricoo@hotmail.com>
2014-02-11 23:19:25 -0800

    pass test for string_scramble

---
+++ lib/string_scramble.rb
@@ -0,0 +1,18 @@
+def string_scramble(string_one, string_two)
+  result = ''
+  string_one_hash = string_to_hash(string_one)
+  string_two_hash = string_to_hash(string_two)
+   result = (string_two_hash.to_a - string_one_hash.to_a).empty?
+   result.to_s
+end
+def string_to_hash(string_input)
+  string_hash = {}
+  string_input.each_char do |character|
+    if string_hash.has_key?(character)
+       string_hash[character] += 1 
+    else
+       string_hash[character] = 0 
+    end   
+  end
+  string_hash
+end
