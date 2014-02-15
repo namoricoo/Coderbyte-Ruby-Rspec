b0cd1cc4a49e500047a39ae5038d047082862d87
Nate <namoricoo@hotmail.com>
2014-02-15 00:28:26 -0800

    pass test to return return string true if any combination of numbers in the array can be added up to equal the largest number

--- lib/array_addition_I.rb
+++ lib/array_addition_I.rb
@@ -1,3 +1,15 @@
-def array_addition_I
-  
+def array_addition_I(input_array)
+  sorted_array = input_array.sort.reverse
+  largest_number = sorted_array[0]
+  total = 0  
+  sorted_array.each_with_index do |number,index|
+    if index > 0
+      if (total + number) == largest_number
+        return 'true'
+      elsif (total + number) < largest_number
+        total += number
+      end      
+    end 
+  end
+  'false' 
 end
--- spec/array_addition_I_spec.rb
+++ spec/array_addition_I_spec.rb
@@ -6,7 +6,7 @@
   For example: if arr contains [4, 6, 23, 10, 1, 3] the output should
   return true because 4 + 6 + 10 + 3 = 23. The array will not be
   empty, will not contain all the same elements, and may contain
-  negative numbers. 
+  negative numbers.
 =end
 require 'spec_helper.rb'
 require_relative '../lib/array_addition_I.rb'
